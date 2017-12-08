62f1dee2616a82c71d1231cace639ab7ca22b663
Chad Montoya <chadmontoya21@yahoo.com>
2017-12-08 21:55:46 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -51,19 +51,19 @@ class MusicLibraryController
   end
 
   def list_songs_for_play
-    Song.all.sort_by! {|s| s.name}.collect.with_index(1) do |s, i|
+    Song.all.sort_by!{|s| s.name}.collect.with_index(1) do |s, i|
        "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
      end
   end
 
   def list_artists
-    Artist.all.sort_by! {|a| a.name}.each.with_index(1) do |a, i|
+    Artist.all.sort_by!{|a| a.name}.each.with_index(1) do |a, i|
        puts "#{i}. #{a.name}"
      end
   end
 
   def list_genres
-    Genre.all.sort_by! {|g| g.name}.each.with_index(1) do |g, i|
+    Genre.all.sort_by!{|g| g.name}.each.with_index(1) do |g, i|
        puts "#{i}. #{g.name}"
      end
   end
